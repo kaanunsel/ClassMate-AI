7d374b5a31764c323643cdbbb15dc2c7e59d77ea
Kaan <kaan.unsel@hotmail.com>
2025-01-15 14:46:52 +0300

    Initial Commit

---
+++ ClassMate AI/ClassMate_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassMate_AIApp.swift
+//  ClassMate AI
+//
+//  Created by Kaan Ünsel on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClassMate_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassMate AITests/ClassMate_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassMate_AITests.swift
+//  ClassMate AITests
+//
+//  Created by Kaan Ünsel on 15.01.2025.
+//
+
+import Testing
+@testable import ClassMate_AI
+
+struct ClassMate_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
